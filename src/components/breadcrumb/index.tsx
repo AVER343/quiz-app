8fcde4e6e49ac58ca8544523c9b7ff61d3b5b926
aver343 <ankushvermagb@gmail.com>
2022-04-29 18:43:19 +0530

    autocomplete

--- src/components/breadcrumb/index.tsx
+++ src/components/breadcrumb/index.tsx
@@ -1,10 +1,10 @@
 import { Breadcrumb, BreadcrumbItem, BreadcrumbLink } from '@chakra-ui/react';
 import { useRouter } from 'next/router';
-import { BsChevronRight } from 'react-icons/bs';
+// import { BsChevronRight } from 'react-icons/bs';
 export function BreadCrumbComponent(props: { breadcrumbs: { href: string; title: string }[] }) {
 	let router = useRouter();
 	return (
-		<Breadcrumb spacing={'8px'} color={'teal'} colorScheme={'cyan'} separator={<BsChevronRight color='gray.500' />}>
+		<Breadcrumb spacing={'8px'} color={'teal'} colorScheme={'cyan'} separator={'/'}>
 			{props.breadcrumbs.map((e, index) => (
 				<BreadcrumbItem key={index}>
 					<BreadcrumbLink onClick={() => router.push(props.breadcrumbs[index].href)}>
